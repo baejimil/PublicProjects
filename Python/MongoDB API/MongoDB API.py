0d8e9c3ffa43f7de4cc7f546d742e7c8df6f24b1
baejimil <68216569+baejimil@users.noreply.github.com>
2020-09-22 12:30:36 +0900

    Update MongoDB API.py

--- Python/MongoDB API/MongoDB API.py
+++ Python/MongoDB API/MongoDB API.py
@@ -9,7 +9,7 @@ class MongoAPI:
     def __init__(self, data):
         log.basicConfig(level=log.DEBUG, format='%(asctime)s %(levelname)s:\n%(message)s\n')
         # self.client = MongoClient("mongodb://localhost:27017/")  # When only Mongo DB is running on Docker.
-        self.client = MongoClient("mongodb://localhost:27017/")     # When both Mongo and This application is running on
+        self.client = MongoClient("mongodb://mymongo_1:27017/")     # When both Mongo and This application is running on
                                                                     # Docker and we are using Docker Compose
 
         database = data['database']
